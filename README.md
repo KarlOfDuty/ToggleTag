b49536a54b7ff284e20c997c401754f6f80143dd
Karl Essinger <xkaess22@gmail.com>
2023-05-15 22:53:09 +0200

    Moved ToggleTag to NWAPI

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
-# ToggleTag [![Build Status](https://jenkins.karlofduty.com/job/ServerMod/job/ToggleTag/job/master/badge/icon)](https://jenkins.karlofduty.com/blue/organizations/jenkins/ServerMod%2FToggleTag/activity) [![Release](https://img.shields.io/github/release/KarlofDuty/ToggleTag.svg)](https://github.com/KarlOfDuty/ToggleTag/releases) [![Downloads](https://img.shields.io/github/downloads/KarlOfDuty/ToggleTag/total.svg)](https://github.com/KarlOfDuty/ToggleTag/releases) [![Discord Server](https://img.shields.io/discord/430468637183442945.svg?label=discord)](https://discord.gg/C5qMvkj)
+# ToggleTag [![Build Status](https://jenkins.karlofduty.com/job/NWAPI/job/ToggleTag/job/master/badge/icon)](https://jenkins.karlofduty.com/blue/organizations/jenkins/NWAPI%2FToggleTag/activity) [![Release](https://img.shields.io/github/release/KarlofDuty/ToggleTag.svg)](https://github.com/KarlOfDuty/ToggleTag/releases) [![Downloads](https://img.shields.io/github/downloads/KarlOfDuty/ToggleTag/total.svg)](https://github.com/KarlOfDuty/ToggleTag/releases) [![Discord Server](https://img.shields.io/discord/430468637183442945.svg?label=discord)](https://discord.gg/C5qMvkj)
 
 An SCP:SL ServerMod plugin which lets you persistently toggle your staff tag using the default `hidetag` and `showtag` commands. You have to use the RA console, not the local one.
 
-Requires you to enable the text based admin commands or use scpdiscord.
-
 Now also persistently toggles overwatch.
-
-Has a single config entry, `toggletag_global` which decides whether the plugin files are placed in the global or local config directory.
 
 ## Installation
 
-Extract the included zip and place the contents in `sm_plugins`.
+Place the plugin and dependencies directory in your server's plugin directory.
+
+The plugin has no config, it starts automatically.
 
 ## Commands
 
-`console_hidetag <steamid>`
+`console_hidetag <userid>`
 
-`console_showtag <steamid>`
+`console_showtag <userid>`
 
 These are not meant to be used directly, they just exist for SCPDiscord integration. You can instead just use the default `hidetag` and `showtag` commands.
-
-## Permissions
-
-`toggletag.savetag` - Allows players to have their tag status saved, given by default.
-
-`toggletag.saveoverwatch` - Allows players to have their overwatch status saved, given by default.
---
+++ ToggleTag.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using System.Linq;
+using CommandSystem;
+using CommandSystem.Commands.RemoteAdmin;
+using Newtonsoft.Json.Linq;
+using PlayerRoles;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using PluginAPI.Helpers;
+using RemoteAdmin;
+
+namespace ToggleTag
+{
+
+    public class ToggleTag
+    {
+        public static HashSet<string> tagsToggled;
+
+        private readonly string defaultConfig =
+        "{\n"                 +
+        "    \"tags\": []\n"  +
+        "}";
+
+        public const string VERSION = "3.1.1";
+
+        [PluginEntryPoint("SCPDiscord", VERSION, "Enables persistant toggling of tags.", "Karl Essinger")]
+        public void OnEnable()
+        {
+            Log.Info("Loading saved players '" + GetConfigPath() + "'...");
+            if (!File.Exists(GetConfigPath()))
+            {
+                File.WriteAllText(GetConfigPath(), defaultConfig);
+            }
+            JToken jsonObject = JToken.Parse(File.ReadAllText(GetConfigPath()));
+
+            tagsToggled = new HashSet<string>(jsonObject.SelectToken("tags")?.Values<string>());
+
+            EventManager.RegisterEvents(this, this);
+            Log.Info("ToggleTag " + VERSION + " enabled!");
+        }
+
+        public static string GetConfigPath()
+        {
+            return Paths.GlobalPlugins.Plugins + "/toggletag.json";
+        }
+
+        public static void SaveTagsToFile()
+        {
+            // Save the state to file
+            StringBuilder builder = new StringBuilder();
+            builder.Append("{\n");
+            builder.Append("    \"tags\":\n");
+            builder.Append("    [\n");
+            foreach (string line in tagsToggled)
+            {
+                builder.Append("        \"" + line + "\"," + "\n");
+            }
+            builder.Append("    ]\n");
+            builder.Append("}");
+            File.WriteAllText(GetConfigPath(), builder.ToString());
+        }
+
+        [PluginEvent(ServerEventType.PlayerJoined)]
+        public void OnPlayerJoin(Player player)
+        {
+            if(tagsToggled.Contains(player.UserId))
+            {
+                player.ReferenceHub.characterClassManager.CmdRequestHideTag();
+            }
+            else
+            {
+                player.ReferenceHub.characterClassManager.CmdRequestShowTag(false);
+            }
+        }
+
+        [PluginEvent(ServerEventType.RemoteAdminCommandExecuted)]
+        public void OnAdminQuery(ICommandSender sender, string command, string[] args, bool result, string response)
+        {
+            if (!(sender is PlayerCommandSender playerSender) || Player.Get(playerSender.ReferenceHub) == null) return;
+
+            Player player = Player.Get(playerSender.ReferenceHub);
+            switch (command)
+            {
+                case "hidetag":
+                    tagsToggled.Add(player.UserId);
+                    SaveTagsToFile();
+                    return;
+                case "showtag":
+                    tagsToggled.Remove(player.UserId);
+                    SaveTagsToFile();
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+
+    [CommandHandler(typeof (GameConsoleCommandHandler))]
+    class HideTagCommand
+    {
+        public string Command => "console_hidetag";
+        public string[] Aliases => new string[] {};
+        public string Description => "Hide the tag of a player, callable from the console.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.Count > 0)
+            {
+                bool wasVisible = ToggleTag.tagsToggled.Add(arguments.At(0));
+
+                Player player = Player.GetPlayers().FirstOrDefault(x => x.UserId == arguments.At(0));
+
+                if (player == null)
+                {
+                    response = "Could not find a player by that user ID.";
+                    return false;
+                }
+
+                player?.ReferenceHub.characterClassManager.CmdRequestHideTag();
+                if (wasVisible)
+                {
+                    response = "Tag hidden of " + player.DisplayNickname + ".";
+                    ToggleTag.SaveTagsToFile();
+                }
+                else
+                {
+                    response = "Tag was already hidden.";
+                }
+
+                return true;
+            }
+            response = "Not enough arguments provided. 'console_hidetag <userid>'";
+            return false;
+        }
+    }
+
+    [CommandHandler(typeof (GameConsoleCommandHandler))]
+    class ShowTagCommand
+    {
+        public string Command => "console_showtag";
+        public string[] Aliases => new string[] {};
+        public string Description => "Show the tag of a player, callable from the console.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.Count > 0)
+            {
+                bool wasHidden = ToggleTag.tagsToggled.Remove(arguments.At(0));
+
+                Player player = Player.GetPlayers().FirstOrDefault(x => x.UserId == arguments.At(0));
+
+                if (player == null)
+                {
+                    response = "Could not find a player by that user ID.";
+                    return false;
+                }
+
+                player.ReferenceHub.characterClassManager.CmdRequestShowTag(false);
+                if (wasHidden)
+                {
+                    response = "Tag revealed of " + player.DisplayNickname + ".";
+                    ToggleTag.SaveTagsToFile();
+                }
+                else
+                {
+                    response = "Tag was already revealed.";
+                }
+
+                return true;
+            }
+            response = "Not enough arguments provided. 'console_showtag <userid>'";
+            return false;
+        }
+    }
+}
